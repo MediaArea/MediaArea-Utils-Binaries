b22221fa46c2545078f139d38aa09fcb7bbae083
Your Name <you@example.com>
2017-10-22 20:36:11 +0200

    Remove old qt versions

--- Windows/Qt/Qt5.5-msvc2013/5.5/msvc2013/bin/qtenv2.bat
+++
@@ -1,5 +0,0 @@
-echo off
-echo Setting up environment for Qt usage...
-set PATH=C:\Programmation\Qt5.5-msvc2013\5.5\msvc2013\bin;%PATH%
-cd /D C:\Programmation\Qt5.5-msvc2013\5.5\msvc2013
-echo Remember to call vcvarsall.bat to complete environment setup!
--- Windows/Qt/Qt5.5-msvc2013_64/5.5/msvc2013_64/bin/qtenv2.bat
+++
@@ -1,5 +0,0 @@
-echo off
-echo Setting up environment for Qt usage...
-set PATH=C:\Programmation\Qt5.5-msvc2013_64\5.5\msvc2013_64\bin;%PATH%
-cd /D C:\Programmation\Qt5.5-msvc2013_64\5.5\msvc2013_64
-echo Remember to call vcvarsall.bat to complete environment setup!
