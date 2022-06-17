ed0507a984a2b5ed172c48fe4e660875d0fdb28e
Maxime Gervais <gervais.maxime@gmail.com>
2022-06-18 01:59:21 +0200

    Remove outdated Qt versions
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Windows/Qt/Qt5.6-msvc2015/5.6/msvc2015/lib/cmake/Qt53DRender/Qt53DRender_GLTFParser.cmake
+++
@@ -1,7 +0,0 @@
-
-add_library(Qt5::GLTFParser MODULE IMPORTED)
-
-_populate_3DRender_plugin_properties(GLTFParser RELEASE "sceneparsers/gltfsceneparser.dll")
-_populate_3DRender_plugin_properties(GLTFParser DEBUG "sceneparsers/gltfsceneparserd.dll")
-
-list(APPEND Qt53DRender_PLUGINS Qt5::GLTFParser)
--- Windows/Qt/Qt5.6-msvc2015_64/5.6/msvc2015_64/bin/qtenv2.bat
+++
@@ -1,5 +0,0 @@
-echo off
-echo Setting up environment for Qt usage...
-set PATH=C:\Programmation\Qt5.6-msvc2015_64\5.6\msvc2015_64\bin;%PATH%
-cd /D C:\Programmation\Qt5.6-msvc2015_64\5.6\msvc2015_64
-echo Remember to call vcvarsall.bat to complete environment setup!
